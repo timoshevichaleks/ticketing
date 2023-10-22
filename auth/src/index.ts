8de7a27d6e532239415f845f968552e8cac5e824
tsimashevick <atsimashevich@elinext.com>
2023-10-22 19:19:39 +0300

    Removed console log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,6 @@ import * as mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
